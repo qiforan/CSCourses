d224afd85901588d06dab303ed8abdb5a1434447
qiforan <1764375147@qq.com>
2021-01-19 18:11:52 +0800

    2021-1-19

--- leetcode/editor/cn/368_largest-divisible-subset.cpp
+++ leetcode/editor/cn/368_largest-divisible-subset.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
+    typedef pair<int, int> pii;
+
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
         sort(nums.begin(), nums.end());
-        
+        pii max_pii = {1, 0};
+        const int n = nums.size();
+        vector<pii> dp(n);
+        for (int i = 0; i < n; i++) {
+            dp[i].first = 1;
+            dp[i].second = i;
+            for (int j = 0; j < i; j++)
+                if (nums[i] % nums[j] == 0 && dp[j].first >= dp[i].first)
+                    dp[i] = {dp[j].first + 1, j};
+            max_pii = max(max_pii, {dp[i].first,i});
+        }
+        vector<int> ret;
+        for(int i = 0,a=max_pii.second; i < max_pii.first; a = dp[a].second,i++)
+            ret.push_back(nums[a]);
+        return ret;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
@@ -15,6 +33,8 @@ public:
 int main()
 {
     Solution s;
+    vector<int> v{1,2,3};
+    s.largestDivisibleSubset(v);
     return 0;
 }
 
